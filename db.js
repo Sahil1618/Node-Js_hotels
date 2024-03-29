0b63a3206ddef279818aaec9d6488b4d2344c8d3
Sahil1618 <ss8876584@gmail.com>
2024-03-29 15:40:00 +0530

    shifted mongodb to online server instead of localhost

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // defining the mongoDB connection URL 
 
-const mongoURL = 'mongodb://127.0.0.1:27017/hotels'
+// const mongoURL = 'mongodb://127.0.0.1:27017/hotels'
+const mongoURL = process.env.MONGODB_URL;
 
 // set up mongoDB Connection
 
