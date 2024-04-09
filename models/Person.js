e8771f482fa839b56623d5c0980d47ea83276e79
Sahil1618 <ss8876584@gmail.com>
2024-04-09 12:52:20 +0530

    authentication in node js

--- models/Person.js
+++ models/Person.js
@@ -1,38 +1,157 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
 
+// const personSchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     age: {
+//         type: Number
+        
+//     },
+//     work: {
+//         type: String,
+//         enum: ['chef','waiter','manager'],
+//         required: true
+//     },
+//     mobile :{
+//         type: String,
+//         required: true
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     },
+//     address: {
+//         type: String,
+//         required: true,
+//     },
+//     salary: {
+//         type: Number,
+//         required: true
+//     },
+//     username: {
+//         required: true,
+//         type: String
+//     },
+//     password: {
+//         type: String,
+//         required: true
+//     }
+// });
+
+// personSchema.pre('save', async function(next){
+//     const person = this;
+
+//     // hash the password only if it has been modified (or is new)
+//     if(!person.isModified('password')) return next();
+
+//     try{
+//         // hash password generation
+//         const salt = await bcrypt.genSalt(10);
+
+//         // hash password
+//         const hashedPassword = await bcrypt.hash(person.password, salt);
+
+//         // override the plain password with the hashed one
+//         person.password = hashedPassword;
+//         next();
+//     }
+//     catch(err){
+//         return next(err);
+//     }
+// })
+
+// personSchema.methods.comparePassword = async function (candidatePassword){
+//     try{
+//         const isMatch = await bcrypt.compare(candidatePassword, this.password);
+//         return isMatch;
+//     }
+//     catch(err){
+//         throw err;
+//     }
+// }
+
+
+// const Person = mongoose.model('Person', personSchema);
+// module.exports = Person;
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// Define the Person schema
 const personSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
     },
-    age: {
+    age:{
         type: Number
-        
     },
-    work: {
+    work:{
         type: String,
-        enum: ['chef','waiter','manager'],
+        enum: ['chef', 'waiter', 'manager'],
         required: true
     },
-    mobile :{
+    mobile:{
         type: String,
         required: true
     },
-    email: {
+    email:{
         type: String,
         required: true,
         unique: true
     },
-    address: {
-        type: String,
-        required: true,
+    address:{
+        type: String
     },
-    salary: {
+    salary:{
         type: Number,
         required: true
+    },
+    username: {
+        required: true,
+        type: String
+    },
+    password: {
+        required: true,
+        type: String
     }
 });
 
+personSchema.pre('save', async function(next){
+    const person = this;
 
+    // Hash the password only if it has been modified (or is new)
+    if(!person.isModified('password')) return next();
+
+    try{
+        // hash password generation
+        const salt = await bcrypt.genSalt(10);
+
+        // hash password
+        const hashedPassword = await bcrypt.hash(person.password, salt);
+        
+        // Override the plain password with the hashed one
+        person.password = hashedPassword;
+        next();
+    }catch(err){
+        return next(err);
+    }
+})
+
+personSchema.methods.comparePassword = async function(candidatePassword){
+    try{
+        // Use bcrypt to compare the provided password with the hashed password
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    }catch(err){
+        throw err;
+    }
+}
+
+// Create Person model
 const Person = mongoose.model('Person', personSchema);
 module.exports = Person;
--- server.js
+++ server.js
@@ -1,24 +1,36 @@
-
 const express = require('express')
-const app = express()
+const app = express();
 const db = require('./db');
 require('dotenv').config();
+const passport = require('./auth');
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());  // req.body
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json()); // req.body
 const PORT = process.env.PORT || 3000;
 
+
+// Middleware Function
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+    next(); // Move on to the next phase
+}
+app.use(logRequest);
+
+app.use(passport.initialize());
+const localAuthMiddleware = passport.authenticate('local', {session: false})
+
 app.get('/', function (req, res) {
-  res.send('Welcome to Node JS server made by Sahil.😀')
+    res.send('Welcome to our Hotel');
 })
 
+// Import the router files
+const personRoutes = require('./routes/personRoutes');
+const menuItemRoutes = require('./routes/menuItemRoutes');
 
-const personRoutes = require('./routes/personRoutes')
-const menuItemRoutes = require('./routes/menuItemRoutes')
-
-app.use('/person', personRoutes)
-app.use('/menu', menuItemRoutes)
-
-app.listen(PORT,()=>{
-    console.log('Listening on port 3000');
+// Use the routers
+app.use('/person', localAuthMiddleware,personRoutes);
+app.use('/menu', menuItemRoutes);
+  
+app.listen(PORT, ()=>{
+    console.log('listening on port 3000');
 })
